c88ad15c9680c2ffecd0a371285a8109f36b660e
novemberrain00 <64091748+novemberrain00@users.noreply.github.com>
2022-04-12 11:59:44 +0300

    Add files via upload
    added "cut itself'

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const keys = document.querySelectorAll('.key');
+let audioPlayed = false;
 
-const startPlaying = key => {
+const startPlaying = (key, e) => {
     const audio = key.querySelector('audio');
+    
+    if(audioPlayed) return;
+
     if(audio.ended) {
         audio.play();
         key.classList.add('key_played');
@@ -10,16 +14,21 @@ const startPlaying = key => {
         audio.pause();
         audio.play();
     }
+
+    audioPlayed = true;
+    
+
 }
 
 const endPlaying = key => {
     key.classList.remove('key_played');
+    audioPlayed = false;
 }
 
 document.addEventListener('keydown', e => {
     keys.forEach(key => {
         if(key.querySelector('.key__name').getAttribute('data-key') == e.which) {
-            startPlaying(key);
+            startPlaying(key, e);
         };
     });
 });
