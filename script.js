c0681a97a694926a2080bfbb7b322e13c041ebf7
Roman <64091748+novemberrain00@users.noreply.github.com>
2022-07-01 02:18:03 +0300

    Add files via upload
    added first version of metronom

--- script.js
+++ script.js
@@ -1,3 +1,103 @@
+const triggers = []
+
+class Button {
+    constructor(elem, callback) {
+        this.elem = elem;
+        this.callback = callback;
+
+        this.elem.addEventListener('click', () => this.callback())
+    }
+}
+
+class Trigger {
+    constructor(sound, posY, posX) {
+        this.sound = sound;
+        this.posX = posX;
+        this.posY = posY;
+        this.block = document.createElement('div');
+
+        this.block.classList.add('metr__trigger');
+    }
+
+    render() {
+        const { sound, block, posX, posY } = this;
+
+        block.style.left = posX + 'px';
+        block.style.top = posY + 'px';
+        document.body.append(block);
+        sound.play();
+
+        triggers.push({
+            posX,
+            sound
+        })
+
+        console.log(triggers)
+
+        block.addEventListener('click', () => {
+            sound.play();
+        });
+    }
+}
+
+const metronomBtn = new Button(document.querySelector('#metr-btn'), () => {
+    document.querySelector('#metronom').classList.remove('modal_closed');
+});
+
+document.querySelectorAll('.modal__closer').forEach((closer, i) => {
+    closer.addEventListener('click', () => {
+        document.querySelectorAll('.modal')[i].classList.add('modal_closed');
+    });
+});
+
+document.querySelectorAll('.metr__line').forEach((line, i) => {
+    const sound = document.querySelectorAll('.metr__road audio')[i];
+    line.addEventListener('click', e => {
+        new Trigger(sound, e.pageY, e.pageX).render();
+    }) 
+})
+
+const metronomPlayBtn = document.querySelector('#metr-play'),
+    metronomSpeed = +document.querySelector('.metr__speed').value;
+
+metronomPlayBtn.addEventListener('click', () => {
+    const endPos = parseInt(window.getComputedStyle(document.querySelector('.metr')).width),
+        parser = document.querySelector('.metr__parser');
+    let startPos = parseInt(window.getComputedStyle(parser).left);
+
+    parser.style.left = '50px';
+
+    const t = setInterval(() => {
+        if(startPos < endPos - 2) {
+            startPos++;
+            parser.style.left = startPos + 'px';
+
+            triggers.forEach(({posX, sound}) => {
+                if(posX === parser.getBoundingClientRect().x) {
+                    sound.play();
+                }
+            });
+
+        } else {
+            clearInterval(t);
+        }
+    }, metronomSpeed);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//старое, пока не трогать
 const keys = document.querySelectorAll('.key');
 let audioPlayed = false;
 
